686fc934fbceb11e12e0db5d258aa4687f12590c
Hagar Hamad <hagar.akrum@gmail.com>
2023-07-20 00:09:36 +0300

    Cart product association donne

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,10 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Cart;
+use App\Models\CartProductMap;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
-    //
+    public function getItems(Request $request)
+    {
+
+    }
+
+    public function addItems(Request $request)
+    {
+        $user = Auth::user();
+
+        $cart = Cart::where('user_id', $user->getAuthIdentifier())->first();
+
+        if ($cart) {
+            foreach ($request->cart as $item) {
+                $cartItem = CartProductMap::where('cart_id', $cart->id)->where('product_id', $item['id'])->first();
+                if ($cartItem) {
+                    $cartItem->quantity += $item['count'];
+                    $cartItem->save();
+                } else {
+                    $newItem = new CartProductMap;
+                    $newItem->product_id = $item['id'];
+                    $newItem->cart_id = $cart->id;
+                    $newItem->quantity = $item['count'];
+                    $newItem->save();
+                }
+            }
+        } else {
+            $cart = new Cart;
+            $cart->user_id = $user->id;
+            $cart->save();
+
+            foreach ($request->cart as $item) {
+                $newItem = new CartProductMap;
+                $newItem->product_id = $item['id'];
+                $newItem->cart_id = $cart->id;
+                $newItem->quantity = $item['count'];
+                $newItem->save();
+            }
+        }
+    }
+
+    public function deleteItems(Request $request)
+    {
+
+    }
 }
